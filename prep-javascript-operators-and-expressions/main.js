3397ae420938232f53ce19d627d59a4f678b0d1e
Ally Greven <allysongreven@gmail.com>
2024-10-02 03:50:08 +0000

    Completed Javascript Operators and Expressions Exercise

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,31 @@
+const number1 = 3;
+const number2 = 7;
+const product = number1 * number2;
+console.log(product);
+
+const charge = 13;
+const payment = 10;
+const amountRemaining = charge - payment;
+console.log(amountRemaining);
+
+const tests = 5;
+const assignments = 10;
+const final = 15;
+let grade = (tests + assignments + final) / 3;
+console.log(grade);
+
+const firstName = 'Ally';
+const lastName = 'Greven';
+const fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+const pH = 7;
+let isAcidic = pH < 7;
+console.log(pH < 7);
+
+let numberOfSoldiers = 129;
+let isSparta = numberOfSoldiers === 300;
+console.log(numberOfSoldiers === 300);
+
+let nickname = fullName + ' ' + 'is the Goat';
+console.log(nickname);
